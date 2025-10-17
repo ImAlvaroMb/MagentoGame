b40b36114b9e1c04831c05a4d68ff0ab092190e0
ImAlvaroMb <alvaro207mb@gmail.com>
2025-10-17 18:31:53 +0200

    added some necessary varaibles to the base magnetobject class

--- Assets/Scripts/MagnetismRelated/MagnetObject.cs
+++ Assets/Scripts/MagnetismRelated/MagnetObject.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 using Enums;
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public class MagnetObject : MonoBehaviour
 {
+    public float MaxMagneticForce => maxMagneticForce;
+    public float AttractionMultiplier => attractionMultiplier;
+    public float RepulsionMultiplier => repulsionMultiplier;
+    public bool IsStatic => isStatic;
+    public MagnetismForceMode MagnetismMode => magnetismMode;
+
+
     [SerializeField] private float maxMagneticForce = 50f;
     [SerializeField] private float attractionMultiplier = 1.0f;
     [SerializeField] private float repulsionMultiplier = 1.0f;
+    [Tooltip("If this object can be moved by the force of the players magnet or not")]
+    [SerializeField] private bool isStatic = false;
+    [Tooltip("What type of force this object is going to apply towards the players magnet")]
+    [SerializeField] private MagnetismForceMode magnetismMode;
+    
+    private Rigidbody2D _rb;
 
-    private Rigidbody _rb;
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody2D>();
+        if (isStatic) _rb.bodyType = RigidbodyType2D.Kinematic;  
     }
 }
